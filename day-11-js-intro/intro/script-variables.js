64b8cac478d0fd0ab648826a7963db5ca6a2a347
Ondřej Hotový <ondrej.hotovy@outlook.com>
2023-05-08 17:00:04 +0200

    day 11 code

---
+++ day-11-js-intro/intro/script-variables.js
@@ -0,0 +1,158 @@
+// "1234"
+
+// let a
+
+// console.log(typeof(123))
+
+// console.log(typeof(typeof(123)))
+
+// 'hello, Peter\'s cat'
+// "hello, Peter's cat"
+
+// 'he said "Hello!"'
+// "he said \"Hello!\""
+
+// "hello\
+// world\
+// !!!!!"
+
+// `hello
+// world
+// !!!!!`
+
+// console.log(Number("123"))
+// console.log(String(123))
+// console.log(Boolean(123))
+
+// let a = 1
+// let b = ' '
+
+// if (a && b) {
+//     console.log("True")
+// }
+
+const thatNumberFromOndrej = 7
+
+// console.log(thatNumberFromOndrej * 6)
+
+let luckyNumber = 7
+// something
+// console.log(luckyNumber);
+
+// bad practice
+// luckyNumber = "Hello"
+// good practice
+const greeting = "Hello"
+
+const DAYS_OF_WEEK = 7
+
+// const testObject = {
+//     value: 7,
+//     doThings: () => {
+//         console.log("Doing things")
+//     }
+// }
+
+// testObject.doThings()
+
+// console.log(greeting.toUpperCase())
+
+// let myArray = [1, 2];
+
+// myArray.push(3)
+
+// console.log(myArray)
+
+// console.log("bye".toUpperCase())
+
+// console.log(2435657865435 / 2)
+
+// console.log(a + " " + b);
+
+// console.log(2 + 2 * 3);
+
+// let i = 0
+
+// // i = i + 1
+// i++
+
+// let number1 = 0;
+// number1++;
+// console.log(number1);
+
+// let number2 = 0;
+// console.log(number2++);
+// console.log(number2)
+// console.log("----------");
+// let number3 = 0;
+// console.log(++number3);
+// console.log(number3)
+
+// i = i * 2
+// i *= 2
+
+// console.log(1 == "1");
+// console.log(1 === Number("1"))
+// console.log(String(1) === "1")
+
+// x > 1
+// !(x > 1)
+// x <= 1
+
+// console.log(false ? "Many Thanks" : "Cheapass")
+
+// const inputReadFromUser = "123"
+
+// price = 10
+
+// console.log("Total price is: "+price)
+
+const sayHi = () => {
+    console.log("Hi")
+}
+
+// sayHi()
+// sayHi()
+// sayHi()
+
+const saySomething = (what) => {
+    console.log(what)
+}
+
+// saySomething("Hello")
+// let what = "Hello"
+// console.log(what)
+// saySomething("Bye")
+
+const sayTwoWords = (first, second) => {
+    console.log(first + " " + second)
+    return "I said two words"
+    console.log("I'm lost and never gonna be in console")
+}
+
+// const result = sayTwoWords("Hi", "Gonzi")
+
+// console.log(result);
+
+// const promptResult = prompt("How are you?")
+// console.log(promptResult);
+
+const myElement = document.querySelector(".hello")
+// const myElement = document.getElementById("greeting")
+
+myElement.innerHTML = "<em>WAKE UP!!!</em>"
+
+console.log(myElement);
+
+// const sayWords = (words) => {
+//     //
+// }
+
+// sayWords(["Hi", "Hello", "Bye"])
+
+// const increaseByOne = (number) => {
+//     console.log(number + 1);
+// }
+
+// increaseByOne(6)
+// increaseByOne(10)
---
+++ day-11-js-intro/intro/script.js
@@ -0,0 +1,14 @@
+// alert("Hello World from an External file")
+
+// console.log("Hello World from an External file")
+console.log("something else")
+console.error("example error")
+
+const myElement = document.querySelector("h1");
+// // do something with myElement
+
+if (myElement == null) {
+    console.error("No element to be found :(")
+}
+
+// console.log(myElement);
