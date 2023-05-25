0048b7bb4335a597265724f36bedb0a2fcfd30bd
Ondřej Hotový <ondrej.hotovy@outlook.com>
2023-05-25 10:17:36 +0200

    day 24 morning workout

---
+++ day-24-php-intro/morning-workout/src/components/CatFact.jsx
@@ -0,0 +1,40 @@
+import {useState, useEffect} from 'react'
+
+
+
+const CatFact = () => {
+
+    const [data, setData] = useState(null)
+    const [dataLoaded, setDataLoaded] = useState(false)
+
+    const fetchData = async () => {
+        const response = await fetch('https://catfact.ninja/fact')
+        const responseData = await response.json()
+        setData(responseData)
+        setDataLoaded(true)
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    return (
+        <div>
+            {!dataLoaded ?
+                <p>Loading</p> :
+                <p>{data.fact}</p>
+            }
+            <br />
+            <button onClick={() => {
+                // might be useful if we WANT the user to know
+                // the code is working on it => they don't think
+                // nothing is happening and don't try to press the button
+                // more times than needed
+                setDataLoaded(false)
+                fetchData()
+            }}>New Fact</button>
+        </div>
+    )
+}
+
+export default CatFact
