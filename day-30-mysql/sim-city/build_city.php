18a1b9408501ee1a281911323df6070689919311
Ondřej Hotový <ondrej.hotovy@outlook.com>
2023-06-02 10:21:24 +0200

    d30 morning workout

---
+++ day-30-mysql/sim-city/build_city.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once "DB.php";
+require_once "DB_functions.php";
+
+require_once "City.php";
+
+$success = DB::connect('localhost', 'world', 'root', '');
+
+// the data about the city being built is in $_POST
+$city_data = $_POST;
+
+
+// modify the class City so it can be used with the DB library
+$city = new City;
+$city->name = $city_data['name'];
+$city->country_id = $city_data['country_id'];
+$city->district = $city_data['district'];
+$city->population = $city_data['population'];
+
+var_dump($city_data);
+echo "<br>";
+var_dump($city);
+// die();
+
+// make sure the $city has the property it needs to have
+
+$city->insert();
